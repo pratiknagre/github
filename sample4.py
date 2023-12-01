0ad6bc1e5afe7d84dbfcc93e72ed20eefecffb91
N1 <pathaknishant003@gmail.com>
2023-12-01 11:53:11 +0530

    add file

---
+++ sample4.py
@@ -0,0 +1,13 @@
+Why Is Data Engineering Important?
+Companies of all sizes have huge amounts of disparate data to comb through to answer critical business questions. Data engineering is designed to support the process, making it possible for consumers of data, such as analysts, data scientists and executives, to reliably, quickly and securely inspect all of the data available.
+
+Data analysis is challenging because the data is managed by different technologies and stored in various structures. Yet, the tools used for analysis assume the data is managed by the same technology and stored in the same structure. This rift can cause headaches for anybody trying to answer questions about business performance.
+
+For example, consider all of the data a brand collects about its customers:
+
+One system contains information about billing and shipping
+Another system maintains order history
+And other systems store customer support, behavioral information and third-party data
+Together, this data provides a comprehensive view of the customer. However, these different datasets are independent, which makes answering certain questions — like what types of orders result in the highest customer support costs — very difficult.
+
+Data engineering unifies these data sets and lets you find answers to your questions quickly and efficiently.
